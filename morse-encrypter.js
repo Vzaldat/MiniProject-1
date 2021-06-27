ea76e51979300a93108c413b65aa7508b717a2aa
Vzaldat <77528903+Vzaldat@users.noreply.github.com>
2021-06-27 14:32:15 +0530

    Add files via upload

---
+++ morse-encrypter.js
@@ -0,0 +1,49 @@
+var morseToenglish = {".-": "a", "-...":"b", "-.-.": "c", "-..": "d", ".":"e", "..-.":"f", "--.":"g", "....":"h", "..":"i", ".---":"j", "-.-":"k", ".-..":"l", "--":"m", "-.":"n", "---":"o", ".--.":"p", "--.-":"q", ".-.":"r", "...":"s", "-":"t", "..-":"u", "...-":"v", ".--":"w", "-..-":"x", "-.--":"y", "--..":"z", ".----":"1", "..---":"2", "...--":"3", "....-":"4", ".....":"5", "-....":"6", "--...":"7", "---..":"8", "----.":"9", "-----":"0", "|":" "};
+
+var decodeMorse = function(morseCode){
+  var words = (morseCode).split(' ');
+  var letters = words.map((w) => w.split(' '));
+  var decoded = [];
+
+  for(var i = 0; i < letters.length; i++){
+    decoded[i] = [];
+    for(var x = 0; x < letters[i].length; x++){
+        if(morseToenglish[letters[i][x]]){
+            decoded[i].push(morseToenglish[letters[i][x]]);
+        }
+     }
+    }
+
+  return decoded.map(arr => arr.join('')).join(' ');
+}
+function Flipobject(object){
+    var EmptObj = {},
+                 temp;
+
+    for(temp in object){
+        EmptObj[obj[temp]]=temp;
+    }
+    return EmptObj;
+}
+
+var encodeMorse = function(englishCode){
+    var words = (morseCode).split(' ');
+  var letters = words.map((w) => w.split(' '));
+  var decoded = [];
+
+  for(var i = 0; i < letters.length; i++){
+    decoded[i] = [];
+    for(var x = 0; x < letters[i].length; x++){
+        if(morseToenglish[letters[i][x]]){
+            decoded[i].push(Flipobject(morseToenglish[letters[i][x]]));
+        }
+    }
+  }
+
+  return decoded.map(arr => arr.join('')).join(' ');
+}
+document.getElementById("Input").innerHTML=prompt("Please enter the English code(Else wait for the next prompt)",inputstring);
+document.getElementById("Output").innerHTML=encodeMorse;
+document.getElementById("Input").innerHTML=prompt("Please enter the Morse code here",inputstring);
+document.getElementById("Output").innerHTML=decodeMorse;
+
